a70cd4541d281982220c084cb235cfe142cc722c
palak-bhatiya <git14hub@gmail.com>
2022-12-06 16:23:00 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <table border="1" align="center" cellpadding="5" cellspacing="5">
+        <tr>
+            <td colspan="10">Cerificate showing the number of mark obtained by &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ID NO.<br></br>Shri/Smt/Kumari of <strong>SIR P.Y SARVAJANIK COLLEGE OF SCIENCE , SURAT <br></br> </strong>in each header of passing at  the <strong>BACHELOR OF SCIENCE (FIFTH SEMESTER) held in APRIL-2016</strong>&nbsp;</td>
+        </tr>
+        <tr>
+            <td rowspan="2">Subject name</td>
+            <td colspan="3">Passing Mark</td>
+            <td colspan="6">Mark Obtained</td>
+        </tr>
+        <tr>
+            <td>External</td>
+            <td>Internal</td>
+            <td>Total</td>
+            <td>External</td>
+            <td>Internal</td>
+            <td>Total</td>
+            <td>GR.</td>
+            <td>GP.</td>
+            <td>Credit</td>
+        </tr>
+        <tr>
+            <td>LANGUAGE THROUGH LITERATURE</td>
+            <td>50/18</td>
+            <td>20</td>
+            <td>70/25</td>
+            <td>19</td>
+            <td>(09)</td>
+            <td>28</td>
+            <td>E</td>
+            <td>5</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; PAPER-VI</td>
+            <td colspan="3"></td>
+            <td colspan="3"></td>
+        </tr>
+        <tr>
+            <td>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; PAPER-VII</td>
+            <td colspan="3"></td>
+            <td colspan="3"></td>
+        </tr>
+        <tr>
+            <td>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; PAPER-VIII</td>
+            <td colspan="3"></td>
+            <td colspan="3"></td>
+        </tr>
+        <tr>
+            <td>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; PAPER-IX</td>
+            <td colspan="3"></td>
+            <td colspan="3"></td>
+        </tr>
+        <tr>
+            <td>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; PAPER-X</td>
+            <td colspan="3"></td>
+            <td colspan="3"></td>
+        </tr>
+        <tr>
+            <td>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; PAPER-XI</td>
+            <td colspan="3"></td>
+            <td colspan="3"></td>
+        </tr>
+        <tr>
+            <td>PHYSICS</td>
+            <td>300/108</td>
+            <td>120</td>
+            <td>420/151</td>
+            <td>(143)</td>
+            <td>(60)</td>
+            <td>(203)</td>
+            <td>E</td>
+            <td>5</td>
+            <td>12</td>
+        </tr>
+        <tr>
+            <td>PHYSICS PRACTICAL</td>
+            <td>120/43</td>
+            <td>60</td>
+            <td>180/65</td>
+            <td>(91)</td>
+            <td>(44)</td>
+            <td>(135)</td>
+            <td>B</td>
+            <td>8</td>
+            <td>6</td>
+        </tr>
+        <tr>
+            <td>ELECTRONICS</td>
+            <td>50/18</td>
+            <td>20</td>
+            <td>70/25</td>
+            <td>(26)</td>
+            <td>(10)</td>
+            <td>(36)</td>
+            <td>D</td>
+            <td>6</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>NATIONAL SERVICE SCHEME</td>
+            <td colspan="8"></td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td colspan="11">&nbsp;</td>
+        </tr>
+        <tr>
+            <th align="right">Aggregate Total</th>
+            <th align="left">520</th>
+            <td></td>
+            <th align="left">740</th>
+            <th align="left">279</th>
+            <td></td>
+            <th align="left">402</th>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th align="right">Results</th>
+            <th colspan="9" align="left">Pass</th>
+        </tr>
+    </table>
+    </>
+  );
+}
+
+export default App;
